f0f4573e7ba70bf796ac9166bd143737b5062a11
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-02 17:23:18 -0300

    configurando as credenciais do banco de dados

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'development' => array(
+        'driver'    => 'mysql',
+        'host'      => 'localhost',
+        'database'  => 'php-004-son-gerenciador-financas-pessoais',
+        'username'  => 'root',
+        'password'  => '',
+        'charset'   => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+    ),
+);
