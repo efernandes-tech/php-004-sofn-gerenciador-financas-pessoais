effec0cd60c7fe5b52c743f20e17cc3195418d92
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-09 15:33:34 -0300

    criando seeders

---
+++ db/seeds/CategoryCostsSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class CategoryCostsSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $categoryCosts = $this->table('category_costs');
+
+        $categoryCosts->insert(array(
+            array(
+                'name' => 'Category 1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ),
+            array(
+                'name' => 'Category 2',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ),
+        ))->save();
+    }
+}
