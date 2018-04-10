154ec825c2a19831384ee965d8acd02380da495f
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-10 16:27:57 -0300

    template para listar categorias de custo

--- public/index.php
+++ public/index.php
@@ -14,15 +14,20 @@ $app = new Application($serviceContainer);
 $app->plugin(new RoutePlugin());
 $app->plugin(new ViewPlugin());
 
-$app->get('/{name}', function(ServerRequestInterface $request) use ($app) {
-    $view = $app->service('view.renderer');
-    return $view->render('test.html.twig', array('name' => $request->getAttribute('name')));
-});
+// $app->get('/{name}', function(ServerRequestInterface $request) use ($app) {
+//     $view = $app->service('view.renderer');
+//     return $view->render('test.html.twig', array('name' => $request->getAttribute('name')));
+// });
 
 $app->get('/home/{name}/{id}', function(ServerRequestInterface $request){
     $response = new \Zend\Diactoros\Response();
     $response->getBody()->write("response com emmiter do diactoros");
     return $response;
+});
+
+$app->get('/category-costs', function() use ($app) {
+    $view = $app->service('view.renderer');
+    return $view->render('category-costs/list.html.twig');
 });
 
 $app->start();
