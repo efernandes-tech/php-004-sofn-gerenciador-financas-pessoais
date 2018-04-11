0fda0ed8efd3900447997b24cf0e9dc40539d9d9
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-11 14:21:35 -0300

    edicao de categoria de custo

--- public/index.php
+++ public/index.php
@@ -2,38 +2,56 @@
 
 use Psr\Http\Message\ServerRequestInterface;
 use SONFin\Application;
+use SONFin\Models\CategoryCost;
+use SONFin\Plugins\DbPlugin;
 use SONFin\Plugins\RoutePlugin;
 use SONFin\Plugins\ViewPlugin;
-use SONFin\Plugins\DbPlugin;
-use SONFin\Models\CategoryCost;
 use SONFin\ServiceContainer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $serviceContainer = new ServiceContainer();
-$app = new Application($serviceContainer);
+$app              = new Application($serviceContainer);
 
 $app->plugin(new RoutePlugin());
 $app->plugin(new ViewPlugin());
 $app->plugin(new DbPlugin());
 
 $app
-    ->get('/category-costs', function() use($app){
-        $meuModel = new CategoryCost();
+    ->get('/category-costs', function () use ($app) {
+        $meuModel   = new CategoryCost();
         $categories = $meuModel->all();
-        $view = $app->service('view.renderer');
-        return $view->render('category-costs/list.html.twig',[
-            'categories' => $categories
+        $view       = $app->service('view.renderer');
+        return $view->render('category-costs/list.html.twig', [
+            'categories' => $categories,
         ]);
     }, 'category-costs.list')
-    ->get('/category-costs/new', function() use($app){
+
+    ->get('/category-costs/new', function () use ($app) {
         $view = $app->service('view.renderer');
         return $view->render('category-costs/create.html.twig');
     }, 'category-costs.new')
-    ->post('/category-costs/store', function(ServerRequestInterface $request) use($app){
+    ->post('/category-costs/store', function (ServerRequestInterface $request) use ($app) {
         $data = $request->getParsedBody();
         SONFin\Models\CategoryCost::create($data);
         return $app->route('category-costs.list');
-    }, 'category-costs.store');
+    }, 'category-costs.store')
+
+    ->get('/category-costs/{id}/edit', function (ServerRequestInterface $request) use ($app) {
+        $view     = $app->service('view.renderer');
+        $id       = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        return $view->render('category-costs/edit.html.twig', [
+            'category' => $category,
+        ]);
+    }, 'category-costs.edit')
+    ->post('/category-costs/{id}/update', function (ServerRequestInterface $request) use ($app) {
+        $id       = $request->getAttribute('id');
+        $category = \SONFin\Models\CategoryCost::findOrFail($id);
+        $data     = $request->getParsedBody();
+        $category->fill($data);
+        $category->save();
+        return $app->route('category-costs.list');
+    }, 'category-costs.update');
 
 $app->start();
