a17e59466d73390f07c123905905ca47951f4299
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-12 16:04:24 -0300

    criando estrutura de contas a receber

--- php-004-son-gerenciador-financas-pessoais.sql
+++ php-004-son-gerenciador-financas-pessoais.sql
@@ -10,36 +10,73 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+DROP TABLE IF EXISTS `bill_receives`;
+CREATE TABLE IF NOT EXISTS `bill_receives` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_launch` date NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `value` float NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+
+INSERT INTO `bill_receives` (`id`, `date_launch`, `name`, `value`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, '2005-02-22', 'Previdência Privada', 679.63, 2, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(2, '1981-07-18', 'Salário', 516.41, 4, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(3, '1979-12-13', 'Previdência Privada', 413.84, 2, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(4, '2013-02-20', 'Tesouro direto', 570.94, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(5, '1998-06-20', 'CDI', 127.89, 2, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(6, '1970-03-29', 'Venda de produtos usados', 918.81, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(7, '2013-03-19', 'Salário', 368.34, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(8, '2001-03-01', 'Salário', 239.97, 4, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(9, '1998-07-08', 'Bolsa de valores', 471.74, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(10, '2007-05-02', 'Bolsa de valores', 316.96, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(11, '1998-10-18', 'Salário', 199.36, 4, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(12, '2004-09-15', 'Bolsa de valores', 197.8, 3, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(13, '2002-01-22', 'Bolsa de valores', 475.48, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(14, '2010-03-29', 'Previdência Privada', 160.4, 3, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(15, '2009-01-13', 'Tesouro direto', 978.43, 3, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(16, '2003-03-01', 'Previdência Privada', 957.97, 3, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(17, '1987-05-20', 'CDI', 376.64, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(18, '2006-12-29', 'CDI', 182.75, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(19, '1993-06-08', 'Salário', 144.93, 1, '2018-04-12 18:59:15', '2018-04-12 18:59:15'),
+(20, '1993-06-25', 'Tesouro direto', 544.77, 3, '2018-04-12 18:59:15', '2018-04-12 18:59:15');
+
 DROP TABLE IF EXISTS `category_costs`;
 CREATE TABLE IF NOT EXISTS `category_costs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8;
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
-INSERT INTO `category_costs` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Luara Helena Duarte', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(2, 'Emiliano Domingues Jr.', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(3, 'Dr. Thalissa Júlia Barreto', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(4, 'Dr. Joaquin Hernani Verdugo', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(5, 'Sr. Felipe Casanova Rico Jr.', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(6, 'Dr. Rafael Maldonado Bezerra Neto', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(7, 'Olívia Valéria Galindo Filho', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(8, 'Sra. Giovana Amanda Pacheco', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(9, 'Sr. Ziraldo Ivan de Oliveira', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(10, 'Dr. Constância Flores Saraiva Sobrinho', '2018-04-09 18:49:08', '2018-04-09 18:49:08'),
-(19, 'Sr. Jerônimo Duarte Galvão Sobrinho', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(20, 'Srta. Adriana Reis Quintana', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(21, 'Dr. Sabrina Valência Jr.', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(22, 'Luis Salgado', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(23, 'Fernando Pacheco Ferraz', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(24, 'João Cortês Aragão', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(25, 'Srta. Malena Paulina Barreto Jr.', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(26, 'Pâmela Thalissa Padilha', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(27, 'Ricardo Lira Neto', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(28, 'Paulina Garcia Bonilha', '2018-04-11 18:56:42', '2018-04-11 18:56:42');
+INSERT INTO `category_costs` (`id`, `name`, `created_at`, `updated_at`, `user_id`) VALUES
+(1, 'Escola', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 2),
+(2, 'Entretenimento', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 2),
+(3, 'Vestuário', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 2),
+(4, 'Vestuário', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 4),
+(5, 'Água', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(6, 'Cartão', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(7, 'Cartão', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(8, 'Cartão', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 2),
+(9, 'Vestuário', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(10, 'Água', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(11, 'Gasolina', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(12, 'Escola', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(13, 'Gasolina', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 2),
+(14, 'Imposto de Renda', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(15, 'Vestuário', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(16, 'Imposto de Renda', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(17, 'Reparos', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(18, 'Imposto de Renda', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1),
+(19, 'Entretenimento', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 3),
+(20, 'Reparos', '2018-04-12 18:59:14', '2018-04-12 18:59:14', 1);
 
 DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE IF NOT EXISTS `migrations` (
@@ -52,8 +89,10 @@ CREATE TABLE IF NOT EXISTS `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `migrations` (`version`, `migration_name`, `start_time`, `end_time`, `breakpoint`) VALUES
-(20180409180718, 'CreateCategoryCosts', '2018-04-09 21:49:08', '2018-04-09 21:49:08', 0),
-(20180411185134, 'CreateUsersTable', '2018-04-11 21:56:29', '2018-04-11 21:56:29', 0);
+(20180409180718, 'CreateCategoryCosts', '2018-04-12 21:59:14', '2018-04-12 21:59:14', 0),
+(20180411185134, 'CreateUsersTable', '2018-04-12 21:59:14', '2018-04-12 21:59:14', 0),
+(20180412182141, 'AddUserToCategoryCosts', '2018-04-12 21:59:14', '2018-04-12 21:59:14', 0),
+(20180412185446, 'CreateBillReceivesTable', '2018-04-12 21:59:14', '2018-04-12 21:59:14', 0);
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
@@ -69,10 +108,17 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Estêvão', 'Delvalle', 'admin@user.com', '123456', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(2, 'Sara', 'Pacheco', 'pamela90@ig.com.br', '123456', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(3, 'Elias', 'Marés', 'abgail39@meireles.com', '123456', '2018-04-11 18:56:42', '2018-04-11 18:56:42'),
-(4, 'Thiago', 'Carmona', 'batista.sophie@terra.com.br', '123456', '2018-04-11 18:56:42', '2018-04-11 18:56:42');
+(1, 'Josefina', 'Feliciano', 'admin@user.com', '$2y$10$WazTITW1bWbNIUmzt0PaGePU8iIUg4UPifEGVdk9fC27zzRRjGJ7K', '2018-04-12 18:59:14', '2018-04-12 18:59:14'),
+(2, 'Maximiano', 'Paes', 'christopher00@verdara.net.br', '$2y$10$x8DDrlgy2TRAoh/xIOlZtep4xprXuQsCg1VmK8ehiXNFnawJ1Tq0C', '2018-04-12 18:59:14', '2018-04-12 18:59:14'),
+(3, 'Ketlin', 'Medina', 'jasmin.azevedo@yahoo.com', '$2y$10$MDIjQijkgo/SEtot7QaQ3uNGvXMLa0ChEUsfJwWr5lnQXVsEneYBW', '2018-04-12 18:59:14', '2018-04-12 18:59:14'),
+(4, 'Vicente', 'Domingues', 'matias09@saraiva.com.br', '$2y$10$o3JHRJ6bF1Bnhmjn1cp9Fufvoj2Tv52cW2h7ruqcAlBfpFOpGGRWK', '2018-04-12 18:59:14', '2018-04-12 18:59:14');
+
+
+ALTER TABLE `bill_receives`
+  ADD CONSTRAINT `bill_receives_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `category_costs`
+  ADD CONSTRAINT `category_costs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
---
+++ src/Models/BillReceive.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SONFin\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillReceive extends Model
+{
+    /**
+     * Mass Assignment.
+     * @var array
+     */
+    protected $fillable = [
+        'date_launch',
+        'name',
+        'value',
+        'user_id',
+    ];
+}
