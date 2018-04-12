6e6d4b0c721f7859d59b805b64ff68a93ca25341
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-04-12 15:06:56 -0300

    testando barreira da area administrativa

--- src/controllers/auth.php
+++ src/controllers/auth.php
@@ -4,23 +4,37 @@ use Psr\Http\Message\ServerRequestInterface;
 
 $app
 
-->get('/login', function (ServerRequestInterface $request) use ($app) {
-    $view = $app->service('view.renderer');;
-    return $view->render('auth/login.html.twig');
-}, 'auth.show_login_form')
-
-->post('/login', function (ServerRequestInterface $request) use ($app) {
-    $view   = $app->service('view.renderer');
-    $auth   = $app->service('auth');
-    $data   = $request->getParsedBody();
-    $result = $auth->login($data);
-    if (!$result) {
+    ->get('/login', function (ServerRequestInterface $request) use ($app) {
+        $view = $app->service('view.renderer');;
         return $view->render('auth/login.html.twig');
-    }
-    return $app->route('category-costs.list');
-}, 'auth.login')
+    }, 'auth.show_login_form')
 
-->get('/logout', function (ServerRequestInterface $request) use ($app) {
-    $app->service('auth')->logout();
-    return $app->route('auth.show_login_form');
-}, 'auth.logout');
+    ->post('/login', function (ServerRequestInterface $request) use ($app) {
+        $view   = $app->service('view.renderer');
+        $auth   = $app->service('auth');
+        $data   = $request->getParsedBody();
+        $result = $auth->login($data);
+        if (!$result) {
+            return $view->render('auth/login.html.twig');
+        }
+        return $app->route('category-costs.list');
+    }, 'auth.login')
+
+    ->get('/logout', function (ServerRequestInterface $request) use ($app) {
+        $app->service('auth')->logout();
+        return $app->route('auth.show_login_form');
+    }, 'auth.logout');
+
+$app->before(function () use ($app) {
+    $route = $app->service('route');
+    $auth  = $app->service('auth');
+
+    $routesWhiteList = [
+        'auth.show_login_form',
+        'auth.login',
+    ];
+
+    if (!in_array($route->name, $routesWhiteList) && !$auth->check()) {
+        return $app->route('auth.show_login_form');
+    }
+});
