c39b2a9ceebfb22c0d32ee745fd0b15f878c6150
edersonlrf <edersonluis.rf@gmail.com>
2018-04-13 15:51:47 -0300

    crud de contas a receber

--- src/Plugins/DbPlugin.php
+++ src/Plugins/DbPlugin.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 use SONFin\Repository\RepositoryFactory;
 use SONFin\Models\User;
 use SONFin\Models\CategoryCost;
+use SONFin\Models\BillReceive;
 
 class DbPlugin implements PluginInterface
 {
@@ -24,6 +25,10 @@ class DbPlugin implements PluginInterface
 
         $container->addLazy('category-cost.repository', function (ContainerInterface $container) {
             return $container->get('repository.factory')->factory(CategoryCost::class);
+        });
+
+        $container->addLazy('bill-receive.repository', function (ContainerInterface $container) {
+            return $container->get('repository.factory')->factory(BillReceive::class);
         });
 
         $container->addLazy('user.repository', function (ContainerInterface $container) {
---
+++ src/helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * DD/MM/YYYY -> YYYY-MM-DD
+ * @param $date
+ */
+function dateParse($date)
+{
+    $dateArray = explode('/', $date);
+    $dateArray = array_reverse($dateArray);
+    return implode('-', $dateArray);
+}
+
+/**
+ * 1.000,50 -> 1000.50
+ * @param $number
+ * @return mixed
+ */
+function numberParse($number)
+{
+    $newNumber = str_replace('.', '', $number);
+    $newNumber = str_replace(',', '.', $newNumber);
+    return $newNumber;
+}
